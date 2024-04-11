a0f5ba1309bc7722b9bd58d3a54337b381de30ca
inderjeetsingh2003 <phenomenalinderjeet2003@gmail.com>
2024-04-11 12:12:29 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,15 @@
-import './index.css'
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import  Login  from './Components/Login';
-import  Signup  from './Components/Signup';
-import  Home  from './Components/Home';
+import "./App.css";
+import TeacherSignup from "./components/TeacherSignup";
+import TeacherLogin from "./components/TeacherLogin";
 
 function App() {
-  return (
-    <>
-    <Router>
-      
-      <Routes>
-        <Route exact path="/" element={ <Home />} />
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/signup" element={<Signup/>} />
-      </Routes>
-     
-    </Router>
-    </>
-  );
+  return <>
+  
+
+
+  {/*<TeacherSignup/>*/}
+<TeacherLogin/>
+  </>;
 }
 
 export default App;
